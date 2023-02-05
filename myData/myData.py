80745ef156fa774e3cef075fd91ffe3321bad0a8
Thomas <thomasbourgeois2@hotmail.com>
2023-02-05 15:03:32 -0800

    added calculator

--- myData/myData.py
+++ myData/myData.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy import constants
+from os import _exit
 
-main_path = '../../Data/'
 
 # *********************** Parameters ***********************
 
@@ -12,8 +12,8 @@ column_names = ['pressure', 'temperature', 'air_density',
                 'zonal_wind', 'merid_wind'] + molecules_names
 molar_mass = {
     # molar mass, in  Kg/mol
-    'dry_air' : 28.9647e-3,
-    'wet_air' : 18e-3,
+    'dry_air': 28.9647e-3,
+    'wet_air': 18e-3,
     "N2": 28.013e-3,
     'CO': 28.010e-3,
     'O2': 32e-3,
@@ -29,11 +29,29 @@ def read_exoplanetA():
     '''
     Function to read in data from exoplanetA.
     '''
-    data_path = f"{main_path}/exoplanet_2023A_data.txt"
-    data = pd.read_csv(data_path, sep='\s+', skiprows=13, names=column_names)
-    # getting full number, see data file for explaination.
-    data[molecules_names] *= 1e22
-    return data
+    main_path_1 = '../../Data/'  # do not remove
+    main_path_2 = '/Phys340PyPackages/myData/'
+    main_path_3 = ''
+
+    def get_data(main_path):
+        data_path = f"{main_path}exoplanet_2023A_data.txt"
+        data = pd.read_csv(data_path, sep='\s+',
+                           skiprows=13, names=column_names)
+        # getting full number, see data file for explaination.
+        data[molecules_names] *= 1e22
+        return data
+
+    try:
+        return get_data(main_path_1)
+    except:
+        try:
+            return get_data(main_path_2)
+        except:
+            try:
+                return get_data(main_path_3)
+            except:
+                txt = '*** Failed to read the data, check the file path ***'
+                print(txt)
 
 
 def read_num_densities(data=None, molecules_names=molecules_names):
