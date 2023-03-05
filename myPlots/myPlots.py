78fa50bd06d8b3a2d9b270cc0bf8f0c85fb1d10b
Thomas <thomasbourgeois2@hotmail.com>
2023-03-04 21:23:06 -0800

    formatted code

--- myPlots/myPlots.py
+++ myPlots/myPlots.py
@@ -4,10 +4,7 @@ import numpy as np
 import pandas as pd
 
 
-
-
 def plot_vs_pressure(x, title=None, xlabel=None, label=None, kind=None, slice=False):
-    
     '''
     Function to make plots againist pressure
 
@@ -18,22 +15,24 @@ def plot_vs_pressure(x, title=None, xlabel=None, label=None, kind=None, slice=Fa
         label (list_like): labels for legend.
         kind (logx, logy, loglog): which kind of plot to use, default linear.
         slice: (bool): True -> auto slices y-data to fit x-data. 
-    
+
     '''
 
     p = read_exoplanetA().pressure/100
-    if slice: 
-        p = p.iloc[:len(x)]    
+    if slice:
+        p = p.iloc[:len(x)]
 
     def plot(xx, l):
 
         if kind:
-            if kind == "logx" or 'semilogx' or 'logX': plt.semilogx(xx, p, label=l)
-            elif kind == "logy" or 'semilogy' or 'logY': plt.semilogy(xx, p, label=l)
-            elif kind == "loglog" or 'log' or 'Log': plt.loglog(xx, p, label=l)
+            if kind == "logx" or 'semilogx' or 'logX':
+                plt.semilogx(xx, p, label=l)
+            elif kind == "logy" or 'semilogy' or 'logY':
+                plt.semilogy(xx, p, label=l)
+            elif kind == "loglog" or 'log' or 'Log':
+                plt.loglog(xx, p, label=l)
         else:
             plt.plot(xx, p, label=l)
-
 
     if type(x) == (list or np.array or pd.Series or tuple):
         for i, arr in enumerate(x):
@@ -49,10 +48,7 @@ def plot_vs_pressure(x, title=None, xlabel=None, label=None, kind=None, slice=Fa
     plt.legend(fontsize=12)
 
 
-
-
-
-def save_plot(name, path= "WriteUp/Figures/", dpi=500, filetype='.jpeg'):
+def save_plot(name, path="WriteUp/Figures/", dpi=500, filetype='.jpeg'):
     '''
     Function to save a plot,
 
