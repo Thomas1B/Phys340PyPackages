56b8f582df12db40ba1f4f1d7f7ce4102435392b
Thomas <thomasbourgeois2@hotmail.com>
2023-03-04 21:10:15 -0800

    updated function to print multiple things wrt to y-axis

--- myPlots/myPlots.py
+++ myPlots/myPlots.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 from ..myData import read_exoplanetA
+import numpy as np
+import pandas as pd
 
 
 
 
-def plot_vs_pressure(x, title=None, xlabel=None, label=None, kind=None, y=False):
+def plot_vs_pressure(x, title=None, xlabel=None, label=None, kind=None, slice=False):
     
     '''
     Function to make plots againist pressure
@@ -15,25 +17,38 @@ def plot_vs_pressure(x, title=None, xlabel=None, label=None, kind=None, y=False)
         xlabel (str): x-axis label.
         label (list_like): labels for legend.
         kind (logx, logy, loglog): which kind of plot to use, default linear.
-        y: (bool): True auto slices y-data to fit x-data. 
+        slice: (bool): True -> auto slices y-data to fit x-data. 
     
     '''
-    p = read_exoplanetA().pressure/100
-    if y: 
-        p = p.iloc[:len(x)]
 
-    if kind:
-        if kind == "logx" or 'semilogx' or 'logX': plt.semilogx(x, p, label=label)
-        elif kind == "logy" or 'semilogy' or 'logY': plt.semilogy(x, p, label=label)
-        elif kind == "loglog" or 'log' or 'Log': plt.loglog(x, p, label=label)
+    p = read_exoplanetA().pressure/100
+    if slice: 
+        p = p.iloc[:len(xx)]    
+
+    def plot(xx):
+
+        if kind:
+            if kind == "logx" or 'semilogx' or 'logX': plt.semilogx(xx, p, label=label)
+            elif kind == "logy" or 'semilogy' or 'logY': plt.semilogy(xx, p, label=label)
+            elif kind == "loglog" or 'log' or 'Log': plt.loglog(xx, p, label=label)
+        else:
+            plt.plot(xx, p, label=label)
+
+    print(type(x[0]))
+
+    if type(x) == (list or np.array or pd.Series):
+        for arr in x:
+            plot(arr)
     else:
-        plt.plot(x, p, label=label)
+        plot(x)
 
     plt.gca().invert_yaxis()
     plt.title(title, fontsize=14)
     plt.ylabel("Pressure $[hPa]$", fontsize=12)
     plt.xlabel(xlabel, fontsize=12)
     plt.grid()
+
+
 
 
 def save_plot(name, path= "WriteUp/Figures/", dpi=500, filetype='.jpeg'):
